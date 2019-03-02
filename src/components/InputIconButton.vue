06254f3ecf549c7f4a22ef6747e638df684ac952
Baku Hashimoto <mail@baku89.com>
2019-03-02 14:35:34 +0900

    Fix name of component InputIconButton

--- src/components/InputIconButton.vue
+++ src/components/InputIconButton.vue
@@ -11,7 +11,7 @@
 import {Component, Prop, Vue} from 'vue-property-decorator'
 
 @Component
-export default class InputCheckbox extends Vue {
+export default class InputIconButton extends Vue {
 	private imgStyles: object = {}
 
 	private mounted() {

7fb353201622a81d74097ffe84d1b1ed43413b0b
Baku <mail@baku89.com>
2019-04-12 16:34:12 +0900

    Add settings modal on top page

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
 	<div class="App">
-		<div class="App__title-bar" @click="onClickMenu">
-			<button class="App__hamburger">
+		<div class="App__title-bar">
+			<button class="App__hamburger" @click="onClickMenu">
 				<Icon
-					class="mark"
+					class="mark menu"
 					:src="isNavOpened ? './assets/icon_cross.svg' : './assets/icon_hamburger.svg'"
 				></Icon>MENU
 			</button>
+			<button class="App__settings" @click="onClickSettings">
+				<Icon class="mark gear" src="./assets/icon_gear.svg"/>
+			</button>
 		</div>
-		<div :class="{App__nav: true, open: isNavOpened}">
+		<div class="App__nav" :open="isNavOpened">
 			<div class="App__nav-content">
 				<Home/>
 			</div>
@@ -16,6 +19,9 @@
 		<div class="App__view">
 			<router-view/>
 		</div>
+		<Modal :show="isSettingsOpened" @close="isSettingsOpened = false">
+			<h2>Settings</h2>
+		</Modal>
 	</div>
 </template>
 
@@ -23,13 +29,16 @@
 import {Component, Vue, Watch} from 'vue-property-decorator'
 
 import Icon from './components/common/Icon.vue'
+import Modal from './components/common/Modal.vue'
 
 import Home from './pages/Home'
 
 @Component({
-	components: {Home, Icon}
+	components: {Home, Icon, Modal}
 })
 export default class App extends Vue {
+	private isSettingsOpened: boolean = false
+
 	private get isNavOpened(): boolean {
 		return this.$route.path === '/'
 	}
@@ -39,6 +48,10 @@ export default class App extends Vue {
 		} else {
 			this.$router.push('/')
 		}
+	}
+
+	private onClickSettings() {
+		this.isSettingsOpened = !this.isSettingsOpened
 	}
 }
 </script>
@@ -57,75 +70,89 @@ html, body
 	width 100%
 	height 100%
 
-.App__nav
-	$dur = 0.3s
-	$easing = cubic-bezier(0.26, 0, 0.17, 1)
-	position fixed
-	top $title-bar-height
-	z-index 1000
-	visibility hidden
-	width 100%
-	height 'calc(100% - %s)' % $title-bar-height
-	border-right 1px solid var(--color-border)
-	background #fff
-	opacity 0
-	transition visibility 0s linear $dur, opacity $dur $easing, transform $dur $easing
-	transform translateY(-2%)
-
-	&.open
-		visibility visible
-		opacity 1
-		transition visibility 0s, opacity $dur $easing, transform $dur $easing
-		transform none
-
-	&:after
-		position absolute
-		top 100%
-		display block
+	&__nav
+		$dur = 0.3s
+		$easing = cubic-bezier(0.26, 0, 0.17, 1)
+		position fixed
+		top $title-bar-height
+		z-index 1000
+		visibility hidden
 		width 100%
-		height 20%
-		background #ddd
-		content ' '
+		height 'calc(100% - %s)' % $title-bar-height
+		border-right 1px solid var(--color-border)
+		background #fff
+		opacity 0
+		transition visibility 0s linear $dur, opacity $dur $easing, transform $dur $easing
+		transform translateY(-2%)
 
-.App__nav-content
-	overflow scroll
-	padding-top 3em
-	height 100%
+		&[open]
+			visibility visible
+			opacity 1
+			transition visibility 0s, opacity $dur $easing, transform $dur $easing
+			transform none
 
-.App__title-bar
-	position fixed
-	top 0
-	left 0
-	z-index 1001
-	width 100%
-	height $title-bar-height
-	background #222222
+		&:after
+			position absolute
+			top 100%
+			display block
+			width 100%
+			height 20%
+			background #ddd
+			content ' '
 
-.App__hamburger
-	width 10em
-	height $title-bar-height
-	background #333
-	color white
-	vertical-align middle
-	letter-spacing 0.3em
-	line-height $title-bar-height
-	cursor pointer
+	&__nav-content
+		overflow scroll
+		padding-top 3em
+		height 100%
+
+	&__title-bar
+		position fixed
+		top 0
+		left 0
+		z-index 1010
+		width 100%
+		height $title-bar-height
+		background #222222
+
+	&__hamburger, &__settings
+		position absolute
+		top 0
+		height $title-bar-height
+		color white
+		vertical-align middle
+		letter-spacing 0.3em
+		line-height $title-bar-height
+		cursor pointer
+
+		&:hover
+			background var(--color-active)
+
+	&__hamburger
+		width 10em
+		background #333
+
+	&__settings
+		right 0
 
 	.mark
 		display inline-block
 		overflow hidden
-		margin-right 0.1em
-		margin-bottom 0.22em
-		width 1.4em
-		height @width
 		vertical-align middle
 		letter-spacing 0
 		line-height 1em
 
-	&:hover
-		background var(--color-active)
+		&.menu
+			margin-right 0.1em
+			margin-bottom 0.22em
+			width 1.4em
+			height @width
 
-.App__view
-	padding-top $title-bar-height
-	height 100%
+		&.gear
+			margin 0 0.4em 0.2em
+			width 2em
+			height @width
+
+	&__view
+		padding-top $title-bar-height
+		height 100%
 </style>
---
+++ src/components/common/Modal.vue
@@ -0,0 +1,51 @@
+<template>
+	<Portal v-if="show">
+		<div class="Modal" @click="$emit('close')">
+			<div class="Modal__frame" @click.stop>
+				<slot/>
+			</div>
+		</div>
+	</Portal>
+</template>
+
+<script lang="ts">
+import {Component, Vue, Prop} from 'vue-property-decorator'
+
+import Portal from './Portal'
+
+@Component({
+	components: {Portal}
+})
+export default class Modal extends Vue {
+	@Prop({type: Boolean, required: true}) private show!: boolean
+
+	mounted() {}
+}
+</script>
+
+<style lang="stylus">
+@import '../../style/config.styl'
+
+.Modal
+	position fixed
+	top 0
+	left 0
+	z-index 1005
+	display flex
+	flex-direction column
+	justify-content center
+	align-items center
+	width 100vw
+	height 100vh
+	background rgba(black 0.4)
+
+	&__frame
+		position relative
+		padding 1em
+		width 50%
+		height 50%
+		border 1px solid var(--color-border-text)
+		border-radius $border-radius
+		background var(--color-bg)
+		box-shadow 0 0 1em 0 rgba(black, 0.1)
+</style>

fdd87cd91e1bc65e2ccc5a6d36e4d4fdd2f23abf
Baku Hashimoto <mail@baku89.com>
2019-04-20 01:10:22 +0900

    Add page Abstract

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+import Abstract from './pages/Abstract.vue'
 import ComponentsList from './pages/ComponentsList.vue'
 import ParameterControl from './pages/ParameterControl'
 import TransformationMatrix from './pages/TransformationMatrix'
@@ -12,6 +13,11 @@ export default new Router({
 	mode: 'history',
 	base: process.env.BASE_URL,
 	routes: [
+		{
+			path: '/abstract',
+			name: 'abstract',
+			component: Abstract
+		},
 		{
 			path: '/components-list',
 			name: 'components-list',

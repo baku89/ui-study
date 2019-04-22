941244256b1149b2b2507f6ce6f43ca062fae2c9
Baku Hashimoto <mail@baku89.com>
2019-04-22 23:40:12 +0900

    Add Settings

---
+++ src/pages/Settings.vue
@@ -0,0 +1,26 @@
+<template>
+	<SelectionManager>
+		<div class="Settings ui">
+			<ParameterList :value="Config" :definition="DefinitionConfig" width="14em"/>
+		</div>
+	</SelectionManager>
+</template>
+
+<script lang="ts">
+import {Component, Prop, Vue, Inject, Watch} from 'vue-property-decorator'
+
+import {DataConfig, DefinitionConfig} from '../core'
+import components from '../components'
+import {DataColor} from '../data'
+import {convertColorElements} from '../util'
+
+@Component({components})
+export default class Settings extends Vue {
+	@Inject({from: 'Config'}) private Config!: DataConfig
+
+	private DefinitionConfig = DefinitionConfig
+}
+</script>
+
+<style lang="stylus" scoped></style>
+
